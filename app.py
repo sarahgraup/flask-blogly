754bec1c09774f7323df3cc91e66b3bace01a954
sarah <sarahgraup@g.ucla.edu>
2023-03-15 15:52:02 -0700

    created user class and html

--- app.py
+++ app.py
@@ -3,12 +3,17 @@
 import os
 
 from flask import Flask
-from models import connect_db
+from models import connect_db, User, db, connect_db
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     "DATABASE_URL", 'postgresql:///blogly')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'its a secret'
+toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
+
+
--- models.py
+++ models.py
@@ -1,1 +1,31 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
 """Models for Blogly."""
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(
+        db.Integer,
+        primary_key = True,
+        autoincrement = True)
+    
+    first_name = db.Column(
+        db.String(20),
+        nullable = False)
+
+    last_name = db.Column(
+        db.String(20),
+        nullable = False)
+    
+    image_url = db.Column(
+        db.String(100))
+
