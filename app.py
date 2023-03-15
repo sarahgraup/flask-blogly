4606181510da1c9c937361f462bd257a90efcda4
Brian Joo <brianhjoo@gmail.com>
2023-03-15 16:44:54 -0700

    finished /users/new routing

--- app.py
+++ app.py
@@ -2,18 +2,59 @@
 
 import os
 
-from flask import Flask
+from flask import Flask, redirect, render_template, request
 from models import connect_db, User, db, connect_db
 from flask_debugtoolbar import DebugToolbarExtension
 
+
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     "DATABASE_URL", 'postgresql:///blogly')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = 'its a secret'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 
 
+@app.get('/')
+def get_root_directory():
+    """ """
+
+    return redirect('/users');
+
+
+@app.get('/users')
+def get_users_page():
+    """  """
+
+    users = User.query.all()
+
+    return render_template('users.html', users=users)
+
+@app.get('/users/new')
+def show_new_user_form():
+    """  """
+
+    return render_template('new.html')
+
+@app.post('/users/new')
+def process_add_form():
+    """  """
+
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form.get('image_url')
+
+    image_url = image_url if image_url else None
+
+    new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+
+    db.session.add(new_user)
+
+    db.session.commit()
+
+    return redirect('/users')
--- models.py
+++ models.py
@@ -1,23 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
+
 def connect_db(app):
-    """Connect to database."""
+    """ Connect to database. """
 
     app.app_context().push()
     db.app = app
     db.init_app(app)
 
-"""Models for Blogly."""
+
 class User(db.Model):
+    """  """
+
     __tablename__ = 'users'
 
     id = db.Column(
         db.Integer,
         primary_key = True,
         autoincrement = True)
-    
+
     first_name = db.Column(
         db.String(20),
         nullable = False)
@@ -25,7 +29,7 @@ class User(db.Model):
     last_name = db.Column(
         db.String(20),
         nullable = False)
-    
+
     image_url = db.Column(
         db.String(100))
 
